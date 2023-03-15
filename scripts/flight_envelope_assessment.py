04a56b742e86bc6fe1a2e8c90f0a875cd42b6c2f
taranto <jordanttaranto@gmail.com>
2023-03-15 12:26:20 -0500

    added vel_cb function

--- scripts/flight_envelope_assessment.py
+++ scripts/flight_envelope_assessment.py
@@ -3,10 +3,9 @@
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
-
 import rospy
 from geometry_msgs.msg import PoseStamped
-
+from mavros_msgs.msg import VFR_HUD
 
 from helper_functions import quaternionToEuler, eulerToQuaternion
 from data_logger import DataLogger
@@ -15,7 +14,9 @@ from data_logger import DataLogger
 # xml parser will need to be updated and maybe take in user input vs reading only gazebo files
 A0 = 0.05984281113
 CLA = 4.752798721
+CLA_STALL = -3.85
 CDA = 0.6417112299
+CDA_STALL = -0.9233984055
 ALPHA_STALL = 0.3391428111
 WINGAREA = 0.6
 AIR_DENSITY = 1.2041
@@ -23,15 +24,20 @@ MASS = 1.5
 G = 9.81
 
 # alpha_stallDeg = np.rad2deg(0.3391428111)
-# cla_stall = -3.85
-# cda_stall = -0.9233984055
+
 
 
 class Visualiser:
+    '''
+    The Visualizer class plots the rolly, pitch, yaw data live and overlays a static plot to compare real-time
+    data vs. bounds
+    '''
     def __init__(self):
+        # initalizes the Flight Envelope Assessment class
         self.roll = 0.0
         self.pitch = 0.0
         self.yaw = 0.0
+        self.velocity = 0.0
         
         self.fig, self.ax = plt.subplots()
         self.colors = ['orange','blue','red']
@@ -44,15 +50,22 @@ class Visualiser:
         #     self.lines.append(line_object)
 
         self.time = []
+        self.velocity_list = []
         self.roll_list = []
         self.pitch_list = []
         self.yaw_list = []
+        self.load_factor_list = []
+        
+        # y-axis
+        self.load_factor_line, = self.ax.plot([], [], label='load factor', color='purple')
 
         self.repeat_length = 100
         subPosition = rospy.Subscriber('mavros/local_position/pose', PoseStamped, self.position_cb)
-    
+        subvfr_hud = rospy.Subscriber('mavros/vfr_hud', VFR_HUD, self.velocity_cb)
+
         # static data 
-        self.static_data = [30, 60, 90, 120, 150]
+        # place the calculated flight envelope here 
+        self.static_data = []
         self.static_x = list(range(len(self.static_data)))
         self.static_line, = self.ax.plot(self.static_x, self.static_data, color='green', label='static_data')
         self.static_x_updated = self.static_x
@@ -61,8 +74,24 @@ class Visualiser:
 
     def plot_init(self):
         self.ax.set_xlim(left=0, right=self.repeat_length)
-        self.ax.set_ylim([-180, 180])
+        self.ax.set_ylim([-5, 5])
         return self.lines
+
+    def velocity_cb(self, msg):
+        self.velocity = msg.airspeed
+        rospy.loginfo(f"speed: {self.velocity:.4g}")
+
+        time_index = len(self.time)
+        self.time.append(time_index+1)
+
+        if time_index > self.repeat_length:
+            self.ax.set_xlim(time_index-self.repeat_length, time_index)
+        else:
+            self.ax.set_xlim(0, self.repeat_length)
+
+        left_x, right_x = self.ax.get_xlim()
+        self.static_x_updated = [x + left_x for x in self.static_x]
+        self.static_line.set_data(self.static_x_updated, self.static_data)
 
     def position_cb(self, msg):
         qx = msg.pose.orientation.x
@@ -74,31 +103,30 @@ class Visualiser:
         self.pitch = np.rad2deg(self.pitch)
         self.yaw = np.rad2deg(self.yaw)
         
-        time_index = len(self.time)
-        self.time.append(time_index+1)
+        # time_index = len(self.time)
+        # self.time.append(time_index+1)
         self.roll_list.append(self.roll)
         self.pitch_list.append(self.pitch)
         self.yaw_list.append(self.yaw)
 
-        if time_index > self.repeat_length:
-            self.ax.set_xlim(time_index-self.repeat_length, time_index)
-        else:
-            self.ax.set_xlim(0, self.repeat_length)
+        # if time_index > self.repeat_length:
+        #     self.ax.set_xlim(time_index-self.repeat_length, time_index)
+        # else:
+        #     self.ax.set_xlim(0, self.repeat_length)
 
         # static data
-        left_x, right_x = self.ax.get_xlim()
-        self.static_x_updated = [x + left_x for x in self.static_x]
-        self.static_line.set_data(self.static_x_updated, self.static_data)
+        # left_x, right_x = self.ax.get_xlim()
+        # self.static_x_updated = [x + left_x for x in self.static_x]
+        # self.static_line.set_data(self.static_x_updated, self.static_data)
 
-        rospy.loginfo(f"{str(self.roll)} {str(self.pitch)} {str(self.yaw)}")
+        rospy.loginfo(f"{self.roll:.3g} {self.pitch:.3g} {self.yaw:.3g}")
 
     def update_plot(self, frame):
         self.lines[0].set_data(self.time, self.roll_list)    
         self.lines[1].set_data(self.time, self.pitch_list)
         self.lines[2].set_data(self.time, self.yaw_list)
-
-        return self.lines
-
+        return self.lines 
+ 
 
 class FlightEnvelopeAssessment():
     '''
@@ -106,11 +134,12 @@ class FlightEnvelopeAssessment():
     of the flight envelope which will then be fed into the supervisor which will set the bounds 
     of the aircraft
     '''
-    def __init__(self, A0, CLA, CDA, ALPHA_STALL, WINGAREA, AIR_DENSITY, MASS, G):
-
+    def __init__(self, A0, CLA, CDA, ALPHA_STALL, WINGAREA, AIR_DENSITY, MASS, G, CLA_STALL, CDA_STALL):
         self.alpha0 = A0
         self.cla = CLA
+        self.cla_stall = CLA_STALL
         self.cda = CDA
+        self.cda_stall = CDA_STALL
         self.alpha_stall = ALPHA_STALL
         self.area = WINGAREA
         self.rho = AIR_DENSITY
@@ -137,9 +166,40 @@ class FlightEnvelopeAssessment():
 
         self.clMax = max(self.coefficientLiftList)
         self.angleListDegrees = np.rad2deg(self.angleList)
+        self.calc_vStall()
 
-    def calc_cl(self, angle):
-        return self.cla * (angle - self.alpha0)
+    def calc_cl(self, angle_of_attack):
+        cl = self.cla * (angle_of_attack - self.alpha0)
+        return cl
+
+    def calc_vStall(self):
+        self.vStall = np.sqrt((2 * self.mass * self.g) / (self.rho * self.area * self.clMax))
+
+    def calc_lift_coefficient_vs_aoa(self):
+        cl_max_index = np.argmax(self.coefficientLiftList)
+        aoa = self.angleListDegrees
+        cl = self.coefficientLiftList
+        clMax = self.clMax
+        return aoa, cl
+
+    def calc_lift_vs_velocity(self):
+        velocities = np.linspace(self.vStall, 1.5 * self.vStall, 100)
+        lift_values = []
+        for v in velocities:
+            self.dynamicPressure = 0.5 * self.rho * v ** 2
+            self.lift = self.clMax * self.area * self.dynamicPressure
+            lift_values.append(self.lift)
+        return velocities, lift_values
+
+    def calc_load_factor_vs_velocity(self):
+        velocities = np.linspace(self.vStall, 1.5 * self.vStall, 100)
+        load_factors = []
+        for v in velocities:
+            self.dynamicPressure = 0.5 * self.rho * v ** 2
+            self.lift = self.clMax * self.area * self.dynamicPressure
+            self.loadFactor = self.lift / (self.mass * self.g)
+            load_factors.append(self.loadFactor)
+        return velocities, load_factors
 
 
 if __name__ == "__main__":
@@ -148,6 +208,7 @@ if __name__ == "__main__":
     plt.close("all")
     rate = rospy.Rate(20)
     time_zero = rospy.get_time()
+    bounds = FlightEnvelopeAssessment(A0, CLA, CDA, ALPHA_STALL, WINGAREA, AIR_DENSITY, MASS, G, CLA_STALL, CDA_STALL)
     vis = Visualiser()
 
     try:
@@ -159,5 +220,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Exiting Flight Envelope Assessment")
 
-    assessment = FlightEnvelopeAssessment(A0, CLA, CDA, ALPHA_STALL, WINGAREA, AIR_DENSITY, MASS, G)
-    print(assessment.clMax)
