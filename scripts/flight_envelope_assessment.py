f7ca907c36e3735fe03995954f943132ac641d5a
taranto <jordanttaranto@gmail.com>
2023-03-24 05:13:58 -0500

    multiple ways of calculating load factor which one is correct???

--- scripts/flight_envelope_assessment.py
+++ scripts/flight_envelope_assessment.py
@@ -21,50 +21,51 @@ class Visualiser:
     '''
     def __init__(self):
         bounds = FlightEnvelopeAssessment(config.A0, config.CLA, config.CDA, config.ALPHA_STALL, config.WINGAREA, config.AIR_DENSITY, config.MASS, config.G, config.CLA_STALL, config.CDA_STALL)
-
         self.roll = 0.0
         self.pitch = 0.0
         self.yaw = 0.0
         self.velocity = 0.0
         self.vertical_acceleration = 0.0
         self.load_factor = 0.0
-
         self.time_list = []
         self.roll_list = []
         self.pitch_list = []
         self.yaw_list = []
-
         self.roll_rate_list = []
         self.pitch_rate_list = []
         self.yaw_rate_list = []
         
         self.velocity_list = []
-        self.velocity_euler_list = []
         self.load_factor_list = []
+
         self.load_factor_list2 = []
+        self.load_factor_list3 = []
+        self.load_factor_list4 = []
 
         self.load_factor_euler_list = []
+        self.velocity_euler_list = []
 
         subPosition = rospy.Subscriber('mavros/local_position/pose', PoseStamped, self.position_cb)
         subvfr_hud = rospy.Subscriber('mavros/vfr_hud', VFR_HUD, self.velocity_cb)
-        subAcceleration = rospy.Subscriber('mavros/imu/data', Imu, self.cl_callback)
+        subAcceleration = rospy.Subscriber('mavros/imu/data', Imu, self.az_callback)
         subRates = rospy.Subscriber('mavros/imu/data', Imu, self.rates_cb)
 
         self.fig, self.ax = plt.subplots()
-        self.colors = ['black', 'red']
-        self.labels = ['Load Factor', 'pitch over bank']
-        self.lines = [self.ax.plot([], [], label=label, color=color, marker='o', linestyle='', markersize=3)[0] for label, color in zip(self.labels, self.colors)]
+        self.colors = ['black', 'red', 'green', 'purple', 'orange']
+        self.labels = ['n1', 'n2', 'n3', 'n4', 'n5']
 
+        self.lines = [self.ax.plot([], [], label=label, color=color, marker='o', linestyle='', markersize=6)[0] for label, color in zip(self.labels, self.colors)]
+    
         static_velocity, static_load_factor = bounds.calc_load_factor_vs_velocity_static()
         self.static_plot(static_velocity, static_load_factor)
 
     def plot_init(self):
         self.ax.set_xlim(left=0, right=25)
-        self.ax.set_ylim([-5, 5])
+        self.ax.set_ylim([-5, 10])
         return self.lines
 
     def velocity_cb(self, msg):
-        self.velocity = msg.airspeed
+        self.velocity = msg.groundspeed
         self.velocity_list.append(self.velocity)
 
     def position_cb(self, msg):
@@ -86,21 +87,43 @@ class Visualiser:
         self.yaw_rate_list.append(self.yaw_rate)
         # print(f"roll_rate: {self.roll_rate:.4} | pitch_rate: {self.pitch_rate:.4} | yaw_rate: {self.yaw_rate:.4}")
 
-    def cl_callback(self, msg):
+    def az_callback(self, msg):
         az = msg.linear_acceleration.z
         self.vertical_acceleration = az
 
 
-    def calc_load_factor2(self, velocity, roll, pitch):
-        lift_val = bounds.calc_lift2(velocity, roll)
-        load_factor = (lift_val/(bounds.mass * (np.cos(pitch) / np.cos(roll)) * bounds.g))
+
+
+
+
+    def calc_load_factor1(self, velocity, pitch):
+        lift_val = bounds.calc_lift(velocity, pitch)
+        load_factor = (lift_val/(bounds.mass * bounds.g))
+        print(f"Load Factor1: {load_factor:.4}")
         return load_factor
 
-    def calc_load_factor(self, velocity, roll):
-        lift_val = bounds.calc_lift(velocity, roll)
-        # load_factor = (lift_val/(bounds.mass * bounds.g))
-        load_factor = (lift_val/(bounds.mass * self.vertical_acceleration))
+    def calc_load_factor2(self, velocity, pitch):
+        lift_val = bounds.calc_lift(velocity, pitch)
+        load_factor = (lift_val/(self.vertical_acceleration))
+        print(f"Load Factor2: {load_factor:.4}")
         return load_factor
+
+    def calc_load_factor3(self, roll, pitch):
+        n_total = (np.cos(roll) / np.cos(pitch))
+        print(f"Load Factor3: {n_total:.4}")
+        return n_total
+
+    def calc_load_factor4(self, roll, pitch):
+        n_bank = 1/np.cos(roll)
+        n_pitch = 1/np.cos(pitch)
+        n_total = np.sqrt(n_bank**2 + n_pitch**2)
+        print(f"Load factor4: {n_total}")
+        return n_total
+
+    def calc_load_factor5(self, roll):
+        n_bank = 1/np.cos(roll)
+        print(f"Load factor5: {n_bank}")
+        return n_bank
 
     def calc_load_factor_euler(self, velocity, roll, acceleration):
         lift_val = bounds.calc_lift(velocity, roll)
@@ -123,37 +146,47 @@ class Visualiser:
         
         return euler_list, load_factor_euler_list
 
-
-
     def update_plot(self, frame):
         self.thinned_velocity_list = self.velocity_list[-10:]
         self.thinned_roll_angle_list = self.roll_list[-10:]
-        self.thinned_load_factor_list = self.load_factor_list[-10:]
         self.thinned_pitch_angle_list = self.pitch_list[-10:]
 
-        self.thinned_roll_rate_list = self.roll_rate_list[-10:]
-        self.thinned_pitch_rate_list = self.pitch_rate_list[-10:]
+        self.thinned_load_factor_list = self.load_factor_list[-10:]
         self.thinned_load_factor_list2 = self.load_factor_list2[-10:]
+        self.thinned_load_factor_list3 = self.load_factor_list3[-10:]
+        self.thinned_load_factor_list4 = self.load_factor_list4[-10:]
 
+        # for vel, roll in zip(self.thinned_velocity_list, self.thinned_pitch_angle_list):
+        #     load_factor = self.calc_load_factor(vel, roll)
+        #     self.thinned_load_factor_list.append(load_factor)
+        #     # print(f"velocity1: {vel:.4} | roll1: {roll:.4} | n1: {load_factor:.4}")
+        #     print(f"load_factor1: {load_factor:.4}")
 
-        for vel, roll in zip(self.thinned_velocity_list, self.thinned_roll_angle_list):
-            load_factor = self.calc_load_factor(vel, roll)
-            self.thinned_load_factor_list.append(load_factor)
-            print(f"actual velocity: {vel:.4} | actual roll: {roll:.4} | actual n: {load_factor:.4}")
-        
-        for vel2, roll2, pitch2 in zip(self.thinned_velocity_list, self.thinned_roll_angle_list, self.thinned_pitch_angle_list):
-            load_factor2 = self.calc_load_factor2(vel2, roll2, pitch2)
-            self.thinned_load_factor_list2.append(load_factor2)
-            print(f"velocity: {vel2:.4} | roll: {roll2:.4} | cos n: {load_factor2:.4}")
+        # for vel2, roll2, pitch2 in zip(self.thinned_velocity_list, self.thinned_roll_angle_list, self.thinned_pitch_angle_list):
+        #     load_factor2 = self.calc_load_factor2(vel2, roll2, pitch2)
+        #     self.thinned_load_factor_list2.append(load_factor2)
+        #     # print(f"velocity2: {vel2:.4} | roll2: {roll2:.4} | pitch2: {pitch2} | load factor2: {load_factor2:.4}")
+        #     print(f"load_factor2: {load_factor2:.4}")
 
+        load_factor1 = self.calc_load_factor1(self.velocity_list[-1], self.thinned_pitch_angle_list[-1])
+        load_factor2 = self.calc_load_factor2(self.velocity_list[-1], self.thinned_pitch_angle_list[-1])
+        load_factor3 = self.calc_load_factor3(self.thinned_roll_angle_list[-1], self.thinned_pitch_angle_list[-1])
+        load_factor4 = self.calc_load_factor4(self.thinned_roll_angle_list[-1], self.thinned_pitch_angle_list[-1])
+        load_factor5 = self.calc_load_factor5(self.thinned_roll_angle_list[-1])
+
+        self.lines[0].set_data(self.thinned_velocity_list[-1], load_factor1)
+        self.lines[1].set_data(self.thinned_velocity_list[-1], load_factor2)
+        self.lines[2].set_data(self.thinned_velocity_list[-1], load_factor3)
+        self.lines[3].set_data(self.thinned_velocity_list[-1], load_factor4)
+        self.lines[3].set_data(self.thinned_velocity_list[-1], load_factor5)
 
         # euler_vel, euler_load_factor = self.predict_eulers()
+        # self.lines[0].set_data(self.thinned_velocity_list, self.thinned_load_factor_list)
+        # self.lines[1].set_data(self.thinned_velocity_list, self.thinned_load_factor_list2)
 
-        self.lines[0].set_data(self.thinned_velocity_list, self.thinned_load_factor_list)
-        self.lines[1].set_data(self.thinned_velocity_list, self.thinned_load_factor_list2)
         return self.lines
  
-    def static_plot(self, static_velocity, static_load_factor):
+    def static_plot(self, static_velocity, static_load_factor):        
         self.bottom_static = [-x for x in static_load_factor]
         self.ax.plot(static_velocity, static_load_factor, color='red', alpha=0.5, label='Limit of N')
         self.ax.plot(static_velocity, self.bottom_static, color='red', alpha=0.5, label='Limit of -N')
@@ -195,6 +228,7 @@ class FlightEnvelopeAssessment():
 
         self.coefficient_lift_list = []
         self.angleList = np.arange(0, ALPHA_STALL, 0.01 * np.pi/180)
+
         for angle in self.angleList:
             self.coefficient_lift = self.calc_cl(angle)
             self.coefficient_lift_list.append(self.coefficient_lift)
@@ -205,15 +239,15 @@ class FlightEnvelopeAssessment():
 
     def calc_cl(self, angle_of_attack):
         clift = self.cla * (angle_of_attack - self.alpha0)
+        # print(f"Coeff. Lift: {clift}")
         return clift
 
-    def calc_lift(self, pitch_angle, velocity):
-        # not dividing by 2 here took it out 
-        lift = (self.calc_cl(pitch_angle) * self.rho * velocity**2 * self.area)
+    def calc_lift(self, velocity, AoA):
+        lift = (self.calc_cl(AoA) * (self.rho * (velocity**2) * .5) * self.area)
         return lift
     
-    def calc_lift2(self, pitch_angle, velocity):
-        lift = (self.calc_cl(pitch_angle) * self.rho * velocity**2 * self.area)
+    def calc_lift2(self, velocity, AoA):
+        lift = (self.calc_cl(AoA) * (self.rho * velocity**2 * .5) * self.area) 
         return lift
 
     def calc_v_stall(self):
